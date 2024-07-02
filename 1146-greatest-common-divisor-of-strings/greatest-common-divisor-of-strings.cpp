dbfd6d0d47233e898c39399e6954ce1de6e6511a
Wenjia-Lu <78611023+Wenjia-Lu@users.noreply.github.com>
2024-07-01 20:35:11 -0400

    Time: 6 ms (26.57%) | Memory: 8.2 MB (97.21%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    bool eq(const string &tmp, const string &str){
-        if (str.size() % tmp.size() != 0) return false;
-        for(size_t i = 0; i < str.size(); i++){
-            if ( tmp[i % tmp.size()] != str[i]) return false;
+    bool eq(const string &s, const string &str, size_t len){
+        size_t size = str.size();
+        for(size_t i = 0; i < size; i++){
+            if ( s[i % len] != str[i]) return false;
         }
         return true;
     }
 
     string gcdOfStrings(string str1, string str2) {
-        string ans;
-        string tmp;
-        size_t i =0;
-        string &s = str2;
-        if (str1.size() < str2.size()){
-            string &s = str1;
-        }
-        while(i < s.size()){
-            tmp += s[i++];
-            
+        size_t ans = 0;
+        size_t len = 0;
+        size_t sz = min(str1.size(), str2.size());
+        while(len++ < sz){
             // t divides s calc
-            if (eq(tmp, str1) && eq(tmp, str2)){
-                ans = tmp;
-            }
+            if(str1.size() % len == 0 && str2.size() % len == 0 
+                && eq(str2, str1, len) && eq(str1, str2, len)) {
+                    ans = len;
+                }
         }
-        return ans;
+        return str1.substr(0,ans);
     }
 };
