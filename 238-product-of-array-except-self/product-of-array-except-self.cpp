def7135b3d945f3a9c2c23c034b90faf372f5a09
Wenjia-Lu <78611023+Wenjia-Lu@users.noreply.github.com>
2024-07-18 20:20:49 -0400

    Time: 25 ms (72.20%) | Memory: 40.4 MB (11.88%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,26 +2,25 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         // keep running product
-        vector<int> forward(nums.size()+2); 
-        vector<int> reverse(nums.size()+2);
+        vector<int> forward(nums.size()); 
+        vector<int> reverse(nums.size());
 
         int head = 0;
-        int tail = nums.size() + 1; // - 1 + 2
+        int tail = nums.size() -1;
 
-        forward[head] = 1; reverse[head] = 1;
-        forward[tail] = 1; reverse[tail] = 1;
+        forward[head] = 1; reverse[tail] = 1;
 
-        while(tail > 1){
+        while(tail > 0){
             // forward[1] = forward[0] * nums[0] = 1 * nums[0];
             forward[head+1] = forward[head] * nums[head];
-            reverse[tail-1] = reverse[tail] * nums[tail-2];
+            reverse[tail-1] = reverse[tail] * nums[tail];
 
             head++; tail--;
         }
 
         vector<int> sol(nums.size());
         for(int i = 0; i < sol.size(); i++){
-            sol[i] = forward[i] * reverse[i+2];
+            sol[i] = forward[i] * reverse[i];
         }
 
         return sol;
