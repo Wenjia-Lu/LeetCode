706dcf45a2b214a3e1ae4f835581320f775a9e14
Wenjia-Lu <78611023+Wenjia-Lu@users.noreply.github.com>
2024-07-18 20:30:08 -0400

    Time: 27 ms (61.78%) | Memory: 39.4 MB (49.41%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,25 +2,28 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         // keep running product
-        vector<int> forward(nums.size()); 
-        vector<int> reverse(nums.size());
+        // vector<int> forward(nums.size()); 
+        int n = nums.size();
+        vector<int> reverse(n);
 
-        int head = 0;
-        int tail = nums.size() -1;
 
-        forward[head] = 1; reverse[tail] = 1;
+        // int head = 0;
+        int m = n - 1;
+        // forward[head] = 1; 
+        reverse[m] = 1;
 
-        while(tail > 0){
+        while(m > 0){
             // forward[1] = forward[0] * nums[0] = 1 * nums[0];
-            forward[head+1] = forward[head] * nums[head];
-            reverse[tail-1] = reverse[tail] * nums[tail];
-
-            head++; tail--;
+            // forward[head+1] = forward[head] * nums[head];
+            reverse[m-1] = reverse[m] * nums[m];
+            m--;
         }
 
-        vector<int> sol(nums.size());
-        for(int i = 0; i < sol.size(); i++){
-            sol[i] = forward[i] * reverse[i];
+        vector<int> sol(n);
+        m = 1;
+        for(int i = 0; i < n; i++){
+            sol[i] = reverse[i] * m;
+            m *= nums[i];
         }
 
         return sol;
