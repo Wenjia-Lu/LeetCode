7e2a11c6277d002a0651df7fa6f3c66337e70367
Wenjia-Lu <78611023+Wenjia-Lu@users.noreply.github.com>
2024-07-18 20:03:12 -0400

    Time: 28 ms (53.68%) | Memory: 40.3 MB (13.56%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,11 +2,15 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         // keep running product
-        vector<int> forward(nums.size()+2, 1); 
-        vector<int> reverse(nums.size()+2, 1);
+        vector<int> forward(nums.size()+2); 
+        vector<int> reverse(nums.size()+2);
 
         int head = 0;
         int tail = nums.size() + 1; // - 1 + 2
+
+        forward[head] = 1; reverse[head] = 1;
+        forward[tail] = 1; reverse[tail] = 1;
+
         while(tail > 1){
             // forward[1] = forward[0] * nums[0] = 1 * nums[0];
             forward[head+1] = forward[head] * nums[head];
