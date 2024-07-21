af21c83ec49beca659904189a8adaeec661de4e1
Wenjia-Lu <78611023+Wenjia-Lu@users.noreply.github.com>
2024-07-20 21:16:40 -0400

    Time: 7 ms (25.78%) | Memory: 12.8 MB (20.47%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
+        
         size_t p1 = 0;
         size_t p2 = 0;
         size_t currLen = 0;
@@ -11,10 +12,17 @@ public:
             if (p2 == chars.size() || chars[p2] != currChar) {
                 p1++;
                 if(currLen > 1){
-                    sLen = to_string(currLen);
-                    for(char c : sLen){
-                        chars[p1++] = c;
+                    size_t k = 0;
+                    while(currLen){
+                        chars[p1++] = '0' + (currLen % 10);
+                        currLen /= 10;
+                        k++;
                     }
+                    reverse(chars.begin() + p1 - k, chars.begin() + p1);
+                    // sLen = to_string(currLen);
+                    // for(char c : sLen){
+                    //     chars[p1++] = c;
+                    // }
                 }
                 if (p2 < chars.size()) {
                     chars[p1] = chars[p2];
