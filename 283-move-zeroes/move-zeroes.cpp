fbd6fb18e5beac3eedfc6a3b34f3b31f80509815
Wenjia-Lu <78611023+Wenjia-Lu@users.noreply.github.com>
2024-07-21 12:34:52 -0400

    Time: 15 ms (68.18%) | Memory: 22 MB (13.43%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         size_t p1 = 0;
-        size_t p2 = 0;
         for(size_t p2 = 0; p2 < nums.size(); p2++){
             if (nums[p2] != 0){
                 swap(nums[p1], nums[p2]);
