4c92afc758aa195824db154f43d9cea7a37f941b
Wenjia-Lu <78611023+Wenjia-Lu@users.noreply.github.com>
2024-07-21 22:35:10 -0400

    Time: 45 ms (34.84%) | Memory: 58.1 MB (17.90%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -6,11 +6,27 @@ public:
         int best = 0;
         int z = 0;
         for(size_t r = 0; r < nums.size(); r++){
-            nums[r] ? ct++ : z++;
+
+            if (nums[r] == 1){
+                ct++;
+            }
+            else { // num == 0
+                z++;
+            }
 
             if (z > k) { // at 3rd zero
-                !nums[l] ? z-- : ct--;
-                l++;
+                // if r is last element & [r] == 0
+                if (r == nums.size() - 1){
+                    best = max(best, ct + min(k, z));
+                    return best;
+                }
+
+                while (nums[l++] != 0){ // no boundck needed i think? cuz [r] must be 0
+                    ct--;
+                }
+
+                // now l must be on a 0
+                z--;
             }
 
             best = max(best, ct + min(k, z));
