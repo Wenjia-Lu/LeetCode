67fbf0cc8f0f673b283058ec31b679b620c66280
Wenjia-Lu <78611023+Wenjia-Lu@users.noreply.github.com>
2024-07-21 22:44:10 -0400

    Time: 48 ms (21.02%) | Memory: 58.2 MB (14.40%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -15,20 +15,12 @@ public:
             }
 
             if (z > k) { // at 3rd zero
-                // if r is last element & [r] == 0
-
-                // dont need this cuz on last loop anyway l "out of bound" doesnt matter
-                // if (r == nums.size() - 1){
-                //     best = max(best, ct + min(k, z));
-                //     return best;
-                // }
-
-                while (nums[l++]){ // no boundck needed i think? cuz [r] must be 0
+                if (nums[l] == 0){
+                    z--;
+                } else {
                     ct--;
                 }
-
-                // now l must be on a 0
-                z--;
+                l++;
             }
 
             best = max(best, ct + min(k, z));
