03414b9287f07daecfdeb0a5c79fb05a2c92ebe9
Wenjia-Lu <78611023+Wenjia-Lu@users.noreply.github.com>
2024-07-26 21:20:58 -0400

    Time: 16 ms (64.71%) | Memory: 34.7 MB (31.15%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -7,16 +7,8 @@ public:
             rightSum += i;
         }
 
-        /*
-        i = 0, num = 1
-        ls = 0, rs = 28
-        
-        i = 1, num = 7
-        ls = 1, rs = 27
-        */
         for(size_t i = 0; i < nums.size(); ++i){
-            if (leftSum == (rightSum - nums[i])) return i;
-            rightSum -= nums[i];
+            if (leftSum == (rightSum - leftSum - nums[i])) return i;
             leftSum += nums[i];
         }
 
