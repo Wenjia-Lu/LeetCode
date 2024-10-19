6269a872268a36790368567834191a6e735695b6
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2024-10-18 21:42:26 -0400

    Time: 15 ms (99.82%) | Memory: 102.1 MB (96.22%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    // if is water, then add 1 to param
+    int isWater(vector<vector<int>>& grid, int r, int c){
+        if (r < 0 || r >= grid.size()) return 1;
+        if (c < 0 || c >= grid[0].size()) return 1;
+        return (grid[r][c] + 1 ) % 2; // return opposite
+    }
+
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int param = 0;
+
+        // find first island
+        for(int r = 0; r < grid.size(); r++){
+            for(int c = 0; c < grid[0].size(); c++){
+                if (grid[r][c]){
+                    param += isWater(grid, r-1, c);
+                    param += isWater(grid, r+1, c);
+                    param += isWater(grid, r, c-1);
+                    param += isWater(grid, r, c+1);
+                }
+            }
+        }
+        return param;
+    }
+};
