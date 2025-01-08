f86b85545b7db75077d1da14ec490f3f7aa5ea0b
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-01-08 17:43:06 -0500

    Time: 11 ms (82.47%) | Memory: 20.8 MB (49.40%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for s in strs:
+            a = ''.join(sorted(s))
+            if a in d:
+                d[a].append(s) 
+            else:
+                d[a] = [s]
+        
+        ans = []
+        for k in d:
+            ans.append(d[k])
+        return ans
+
+
+        
