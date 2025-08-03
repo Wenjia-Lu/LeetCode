4038886f5e4f9ac464d3eed92f67a64ae94ecd32
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-08-03 16:10:23 -0400

    Time: 416 ms (94.52%) | Memory: 20.7 MB (36.29%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,27 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        ans = []
         nums.sort()
-
-        for i, a in enumerate(nums):
-            if a > 0:
+        print(nums)
+        for i, n in enumerate(nums):
+            if n > 0:
                 break
-
-            if i > 0 and a == nums[i - 1]:
+            if i > 0 and n == nums[i-1]:
                 continue
-
-            l, r = i + 1, len(nums) - 1
+            l = i + 1
+            r = len(nums) - 1
             while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
+                m = nums[l] + nums[r] + n
+                if  m > 0:
                     r -= 1
-                elif threeSum < 0:
+                    # while r > l and nums[r] == nums[r+1]:
+                    #     r-=1
+                elif m < 0:
                     l += 1
+                    # while l < len(nums) and nums[l] == nums[l-1]:
+                    #     l+=1
                 else:
-                    res.append([a, nums[l], nums[r]])
+                    item = [nums[l], nums[r], n]
+                    ans.append(item)
                     l += 1
-                    r -= 1
-                    while nums[l] == nums[l - 1] and l < r:
+                    while l < len(nums) and nums[l] == nums[l-1]:
                         l += 1
-                        
-        return res
+                    while r > l and nums[r-1] == nums[r]:
+                        r -= 1
+        return ans
+
