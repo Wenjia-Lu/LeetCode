14a25415e2b6ff904f87a976fb35cc7c95de41b2
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-09-19 02:14:29 -0400

    Time: 7 ms (51.42%) | Memory: 21.3 MB (89.51%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -2,13 +2,17 @@ class MinStack:
     def __init__(self):
         self.stack = []
         self.history = []
+        self.len_s = 0
+        self.len_h = 0
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        self.len_s += 1
         if len(self.stack) == 1:
             self.history.append(val)
         else:
             self.history.append(min(val, self.history[-1]))
+        self.len_h += 1
         
 
 
@@ -16,14 +20,16 @@ class MinStack:
         self.stack.pop()
         self.history.pop()
 
+        self.len_s -= 1
+        self.len_h -= 1
 
 
     def top(self) -> int:
-        return self.stack[-1]
+        return self.stack[self.len_s-1]
         
         
     def getMin(self) -> int:
-        return self.history[-1]
+        return self.history[self.len_h-1]
         
 
 
