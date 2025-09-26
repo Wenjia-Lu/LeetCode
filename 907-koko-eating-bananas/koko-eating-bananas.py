ce0d79c6019cc8f38c83cebba480b1e0b65a40b9
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-09-26 18:24:32 -0400

    Time: 214 ms (12.51%) | Memory: 19.3 MB (10.55%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -13,18 +13,12 @@ class Solution:
         while l < r:
             mid = l + (r-l)//2
             time_spent = self.eating_hours(piles, mid)
-            # print(f"For a speed of {mid}, ate for {time_spent} hours")
-            # print(f"l,r ={(l,r)}")
-            # print(prev, mid)
-            if prev == mid: 
-                # print("converged!")
-                return int(mid)
-            prev = mid
+            # if prev == mid:
+            #     return int(mid)
+            # prev = mid
 
             if time_spent > h: # ate too slow
                 l = mid + 1
-            elif time_spent == h:
-                r = mid
             else:
                 r = mid
         return int(l)
