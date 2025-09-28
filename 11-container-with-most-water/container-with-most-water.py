36e529493b731930d43616fe9a07dde21e0bc708
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-09-28 16:37:19 -0400

    Time: 66 ms (92.77%) | Memory: 28.4 MB (68.06%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -3,10 +3,11 @@ class Solution:
         l, r = 0, len(height) - 1
         water = 0
         while l < r:
-            water = max(water, (r-l) * min(height[l], height[r]))
             if height[l] < height[r]:
+                water = max(water, (r-l) * height[l])
                 l += 1
             else:
+                water = max(water, (r-l) * height[r])
                 r -= 1
         return water
         
