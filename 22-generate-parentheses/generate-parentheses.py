43940feed13c75e24fd82be1b8a1bfe6948e2cf8
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-09-30 03:15:44 -0400

    Time: 3 ms (29.93%) | Memory: 18 MB (69.66%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -3,15 +3,12 @@ class Solution:
         def dfs(left, right, s):
             if len(s) == n * 2:
                 res.append(s)
-                return
-            
+
             if left < n:
-                dfs(left+1, right, s+'(')
+                dfs(left + 1, right, s + "(")
             if right < left:
-                dfs(left, right + 1, s+')')
+                dfs(left, right + 1, s + ")")
+        
         res = []
-        dfs(0,0,'')
+        dfs(0, 0, '')
         return res
-                
-        
-        
