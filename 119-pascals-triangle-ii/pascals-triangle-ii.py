b4e9f1833fd1057bf70dac9fdd8cbbbff8ef4044
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-10-02 12:32:34 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (32.88%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        above = self.getRow(rowIndex - 1)
+        curr = [1]
+        for i in range(len(above) - 1):
+            curr.append(above[i] + above[i+1])
+        curr.append(1)
+        return curr
+
+# recurrence: an = pascal(an-1)
