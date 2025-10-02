b9f75f467ad574b3cd0bba4af8140ef3f510378b
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-10-02 10:55:11 -0400

    Time: 47 ms (74.18%) | Memory: 34.3 MB (51.13%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -4,10 +4,9 @@ class Solution:
         nset = set(nums)
         for n in nset:
             if n - 1 not in nset:
-                curr = 1
-                while n + 1 in nset:
-                    curr += 1
-                    n += 1
-                best = max(best, curr)
+                next = n + 1
+                while next in nset:
+                    next += 1
+                best = max(best, next - n)
         return best
 
