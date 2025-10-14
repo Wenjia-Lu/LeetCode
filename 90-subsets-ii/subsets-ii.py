843439ca935986bf2859bc179ee6ee01c3099f34
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-10-13 20:32:07 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (37.30%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -2,20 +2,26 @@ class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         result = []
+        curr = []
+        # time = 2^n iterations with max n ops per iter = n 2^n
+        # not sure if we worry about sort() time complexity
+        # space = curr = O(n) axill, stack space = O(n)
 
-        def dfs(i, curr):
+        def dfs(i):
             if i == len(nums):
-                result.append(curr)
+                result.append(curr.copy())
                 return
 
             # take current
             n = nums[i]
-            dfs(i+1, curr + [n])
+            curr.append(n)
+            dfs(i+1)
+            curr.pop()
             # not take current
             while i < len(nums) and nums[i] == n:
                 i += 1
-            dfs(i, curr)
+            dfs(i)
 
-        dfs(0,[])
+        dfs(0)
         return result
         
