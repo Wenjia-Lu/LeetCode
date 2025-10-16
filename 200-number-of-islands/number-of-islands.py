7e6875be43a0e73279cb579b689025dd6650ea7b
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-10-16 00:13:56 -0400

    Time: 244 ms (61.88%) | Memory: 20.3 MB (57.57%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -15,11 +15,9 @@ class Solution:
         result = 0
 
         directions = [[-1, 0],[1,0],[0,-1],[0,1]]
-        def visit(r,c):
-            q = collections.deque()
-            q.append((r,c))
-            grid[r][c] = "0"
+        q = collections.deque()
 
+        def visit(r,c):
             while q:
                 a, b = q.popleft() # get current land
 
@@ -29,11 +27,12 @@ class Solution:
                         q.append((r,c))
                         grid[r][c] = "0"
 
-
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == "1":
                     result += 1
+                    q.append((r,c))
+                    grid[r][c] = "0"
                     visit(r,c)
         return result
         
