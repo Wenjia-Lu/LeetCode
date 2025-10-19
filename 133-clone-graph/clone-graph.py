b75a55f1dd59f671957e42b81133bdae6e03f006
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-10-19 19:26:07 -0400

    Time: 52 ms (5.83%) | Memory: 18.1 MB (56.78%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -9,7 +9,28 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        return copy.deepcopy(node)
+        if not node:
+            return None
+        d = {}
+
+        q = collections.deque()
+        q.append(node)
+        d[node] = Node(val=node.val)
+
+        seen = set()
+        while q:
+            n = q.popleft()
+            for neighbor in n.neighbors:
+                if neighbor not in seen:
+                    d[neighbor] = Node(val=neighbor.val)
+                    seen.add(neighbor)
+                    q.append(neighbor)
+                d[n].neighbors.append(d[neighbor])
+        
+        print(d)
+        return d[node]
+
+
 
 
 
