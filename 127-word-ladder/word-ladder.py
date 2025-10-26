3ba0fe0d3706d38e2d8ee87a17a5a9251bac0134
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-10-26 07:27:28 -0400

    Time: 5142 ms (11.52%) | Memory: 18.8 MB (85.38%) - LeetSync

---
+++ 127-word-ladder/word-ladder.py
@@ -0,0 +1,42 @@
+class Solution:
+    def canTransform(self, s1, s2):
+        count = 0
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                count += 1 
+            if count > 1:
+                # print(f"cant transform {s1}, {s2}")
+                return False
+        # print(f"can transform {s1}, {s2}")
+        return True
+
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        words = set(wordList)
+        if endWord not in words:
+            return 0
+        q = collections.deque([(beginWord, 1)])
+        width = 1
+        seen = set()
+        seen.add(beginWord)
+
+        while q:
+            k = width
+            # print(f"loop {k} times: ")
+            width = 0
+            curr, layer = q.popleft()
+            # print(f"layer {layer}: {curr}")
+            for word in words:
+                if word in seen: continue
+                if self.canTransform(curr, word):
+                    if word == endWord:
+                        return layer + 1
+                    seen.add(word)
+                    q.append((word, layer + 1))
+                    # print("added ", word)
+                    width += 1
+        return 0
+
+
+        
+
+        
