33f3157024b0a9021d0965fa8458aa3c7d081638
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-10-26 02:06:25 -0400

    Time: 7 ms (49.52%) | Memory: 18.9 MB (74.41%) - LeetSync

---
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,38 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)] # init each node's parent as itself
+        self.size = [1 for _ in range(n)]
+        self.components = n
+        
+    def union(self, a, b):
+        A, B = self.find(a), self.find(b)
+        if A != B: # if parents arent the same
+            self.components -= 1
+            # optimization here: make smaller tree attach to larger tree!
+            if self.size[A] < self.size[B]:
+                self.parent[A] = B
+                self.size[B] += self.size[A]
+            else:
+                self.parent[B] = A
+                self.size[A] += self.size[B]
+                
+    def find(self, a):
+        if self.parent[a] == a:
+            return a
+        self.parent[a] = self.find(self.parent[a]) # optimization - flatten the tree for search
+        return self.parent[a]
+    
+    def getComp(self):
+        # return len(set(self.parent))
+        return self.components
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        dsu = DSU(n)
+        for u, v in edges:
+            dsu.union(u,v)
+        
+        return dsu.getComp()
+
+
+        
