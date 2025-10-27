260682279a55cdf98ecd9baec71ce7355ae230ec
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-10-27 00:48:08 -0400

    Time: 11 ms (52.83%) | Memory: 19.5 MB (44.64%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
@@ -3,14 +3,14 @@ class Solution:
         perRow = []
         res = 0
         for s in bank:
-            perRow.append(s.count("1"))
+            a = s.count("1")
+            if a > 0:
+                perRow.append(a)
         n = len(perRow)
-        for row, num in enumerate(perRow):
-            nextRow = row + 1
-            while nextRow < n:
-                if perRow[nextRow] > 0:
-                    res += num * perRow[nextRow]
-                    break
-                nextRow += 1
+        l, r = 0, 1
+        while r < n:
+            res += perRow[l] * perRow[r]
+            l, r = r, r+1
+        
         return res
         
