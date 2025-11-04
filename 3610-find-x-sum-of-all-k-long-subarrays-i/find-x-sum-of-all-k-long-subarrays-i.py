e2cc2d790768e45ddfb85044f0100fdcdede2bfd
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2025-11-03 22:27:00 -0500

    Time: 31 ms (33.77%) | Memory: 17.8 MB (59.74%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
@@ -7,12 +7,9 @@ class Solution:
             heapq.heappush(heap, (freq, n))
             if len(heap) > x:
                 heapq.heappop(heap)
+    
         
-        res = 0
-        for freq, n in heap:
-            res += (freq * n)
-        
-        return res
+        return sum(h[1] * h[0] for h in heap)
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
         res = []
         for i in range(len(nums) - k + 1):
