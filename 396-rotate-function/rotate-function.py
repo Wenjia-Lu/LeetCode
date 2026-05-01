79375daa1d2f2e98366fe662e2e935f26b9737b9
Wen <78611023+Wenjia-Lu@users.noreply.github.com>
2026-05-01 16:16:06 -0400

    Time: 125 ms (92.97%) | Memory: 31.5 MB (22.32%) - LeetSync

--- 396-rotate-function/rotate-function.py
+++ 396-rotate-function/rotate-function.py
@@ -1,10 +1,5 @@
 class Solution:
     def maxRotateFunction(self, nums: List[int]) -> int:
-        # F(0) = [1 2 3 4] -> 1 * 0, 2 * 1
-        # F(1) = [4 1 2 3] -> 1 * 1, 2 * 2,
-        # F(2) = [3 4 1 2] -> 1 * 2, 2 * 3
-        # F(3) = [2 3 4 1] -> 1 * 3, 2 * 0
-
         # F(0) = 0X + 1Y + 2Z
         # F(1) = 1X + 2Y + 0Z <- + X + Y clear Z
         # F(2) = 2X + 0Y + 1Z <- + X clear Y + Z
@@ -12,7 +7,6 @@ class Solution:
         # init = 1X + 1Y + 1Z
         sum_ = sum(nums)
         n = len(nums)
-        clear = n - 1
 
         #calc F_0
         curr = 0
@@ -20,10 +14,9 @@ class Solution:
             curr += (i * num)
 
         highest = curr
-        for i in range(len(nums)-1):
+        for clear in range(n-1, 0, -1):
             curr += sum_
             curr -= (nums[clear] * n)
-            clear -= 1
             highest = max(highest, curr)
         return highest
         
